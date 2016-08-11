0a01ad77bbe0adae5dbfa2f929ff341b48bca42c
Alexis Breust <abreust@wanadev.fr>
2016-08-11 11:55:05 +0200

    Initial grunt template

---
+++ tasks/strex.js
@@ -0,0 +1,47 @@
+/*
+ * grunt-strex
+ * https://github.com/wanadev/grunt-strex
+ *
+ * Copyright (c) 2016 Wanadev
+ * Licensed under the BSD-3-Clause license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.registerMultiTask('strex', 'Grunt plugin to extract strings from javascript files, process them and export them all to an other file.', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+
+};
